4b8b5420aedc41e650682f62a096e2ad29f61699
Marci Clark <marcig612@gmail.com>
2019-02-01 02:46:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  
+  class Dog
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
+
+fido= Dog.new("mutt")
