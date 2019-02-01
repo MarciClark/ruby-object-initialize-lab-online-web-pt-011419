ea89feb3abf24c106a681f2a7ee6cc49f3313ee0
Marci Clark <marcig612@gmail.com>
2019-02-01 02:51:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,27 +24,3 @@ end
 end
 
 fido= Dog.new("mutt")
-
-
-
-
-class Dog
-  def initialize (dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name=(dogs_name, dogs_breed = "Mutt")
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name
-    @name
-  end
-  def breed
-    @breed
-  end 
-end 
-  
-  fido = Dog.new("Fido")
